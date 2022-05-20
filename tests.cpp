664fe994cfd8902d93aafb60338ed674bdd4ec31
SexualWhiteChocolate <karimullin.tr@phystech.edu>
2022-05-21 01:04:14 +0300

    Some tests

--- tests.cpp
+++ tests.cpp
@@ -16,14 +16,132 @@ void check(graph<graph<int>> g) {
 }
 
 int main() {
-    graph<string> temp(3);
-    temp.Push_Edge("HUI", 1, 1);
-    temp.Push_Edge("DA", 2, 2);
-    temp.Push_Edge("PIZDA", 3, 2);
-    temp.Push_Edge("NET", 3, 3);
-    cout << temp.Pop_Edge(2, 2) << endl;
+    graph<int> *g1 = NULL;
+	graph<int> *g2 = NULL;
+	
+	try {
+		g1 = new graph<int>(3);
+	} catch (...) {
+		cout << "Test_1\t->\tFAILED" << endl;
+	}
+	cout << "Test_1\t->\tPASSED" << endl;
+	
+	try {
+		g2 = new graph<int>();
+	} catch (...) {
+		cout << "Test_2\t->\tFAILED" << endl;
+	}
+	cout << "Test_2\t->\tPASSED" << endl;
 
-    cout << temp;
-    return 0;
+	try {
+		g1->Pop_Edge(1, 1);
+	} catch (const graph<int>::GRAPH_ERR &ex) {
+		if (ex == graph<int>::EABSENCE)
+			cout << "Test_3\t->\tPASSED" << endl;
+		else
+			cout << "Test_3\t->\tFAILED" << endl;
+	} catch (...) {
+		cout << "Test_3\t->\tFAILED" << endl;
+	}
+
+	try {
+		g1->Push_Edge(1, 1, 1);
+	} catch (...) {
+		cout << "Test_4\t->\tFAILED" << endl;
+	}
+	cout << "Test_4\t->\tPASSED" << endl;
+
+	try {
+		g1->Push_Edge(2, 4, 4);
+	} catch (...) {
+		cout << "Test_5\t->\tFAILED" << endl;
+	}
+	cout << "Test_5\t->\tPASSED" << endl;
+
+	try {
+		*g2 = *g1;
+	} catch (...) {
+		cout << "Test_6\t->\tFAILED" << endl;
+	}
+	cout << "Test_6\t->\tPASSED" << endl;
+
+	check(*g1);
+	check(*g2);
+
+	try {
+		if (g2->Pop_Edge(1, 1) != 1)
+			cout << "Test_7\t->\tFAILED" << endl;
+	} catch (...) {
+		cout << "Test_7\t->\tFAILED" << endl;
+	}
+	cout << "Test_7\t->\tPASSED" << endl;
+
+	try {
+		if (g2->Pop_Edge(4, 4) != 2)
+			cout << "Test_8\t->\tFAILED" << endl;
+	} catch (...) {
+		cout << "Test_8\t->\tFAILED" << endl;
+	}
+	cout << "Test_8\t->\tPASSED" << endl;
+
+	try {
+		g2->Pop_Edge(1, 2);
+	} catch (const graph<int>::GRAPH_ERR &ex) {
+                if (ex == graph<int>::EABSENCE)
+			cout << "Test_9\t->\tPASSED" << endl;
+		else
+			cout << "Test_9\t->\tFAILED" << endl;
+	} catch (...) {
+		cout << "Test_9\t->\tFAILED" << endl;
+	}
+
+	graph<graph<int>> *g3 = NULL;
+
+	try {
+		g3 = new graph<graph<int>>(2);
+	} catch (...) {
+		cout << "Test_10\t->\tFAILED" << endl;
+	}
+	cout << "Test_10\t->\tPASSED" << endl;
+
+	try {
+		g3->Push_Edge(*g2, 1, 1);
+	} catch (...) {
+		cout << "Test_11\t->\tFAILED" << endl;
+	}
+	cout << "Test_11\t->\tPASSED" << endl;
+
+	try {
+		g3->Push_Edge(*g1, 2, 2);
+	} catch (...) {
+		cout << "Test_12\t->\tFAILED" << endl;
+	}
+	cout << "Test_12\t->\tPASSED" << endl;
+
+	check(*g3);
+
+	graph<int> tmp;
+	try {
+		tmp = g3->Pop_Edge(1, 1);
+	} catch (...) {
+		cout << "Test_13\t->\tFAILED" << endl;
+	}
+	cout << "Test_13\t->\tPASSED" << endl;
+
+	int val = 0;
+	try {
+		val = tmp.Pop_Edge(1, 1);
+	} catch (...) {
+		cout << "Test_14\t->\tFAILED" << endl;
+	}
+	if (val == 1)
+		cout << "Test_14\t->\tPASSED" << endl;
+	else
+		cout << "Test_14\t->\tFAILED" << endl;
+
+	delete g1;
+	delete g2;
+	delete g3;
+	return 0;
 }
 
